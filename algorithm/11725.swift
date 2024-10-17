fdecdad4f6ee71f653ab6800e84212c6bcbcd1a6
sikwan <yysskk99999@naver.com>
2024-10-17 15:58:29 +0900

    variable add

---
+++ algorithm/11725.swift
@@ -0,0 +1,17 @@
+//
+//  11725.swift
+//  algorithm
+//
+//  Created by 양시관 on 10/17/24.
+//
+
+import Foundation
+
+
+var N = Int(readLine()!)!
+
+var node = readLine()?.split(separator: " ").map{Int($0)} // 배열 생성해서 넣어준거고 ,
+
+
+
+
