66ec2e14eb93f1db3fa7419cd78891aee0d6a866
sikwan <yysskk99999@naver.com>
2024-10-24 19:19:10 +0900

    2164

--- algorithm/main.swift
+++ algorithm/main.swift
@@ -7,31 +7,25 @@
 
 import Foundation
 
-let queue = readLine()!.split(separator: " ").map{Int($0)!}
 
 
-var N = queue[0]
-var K = queue[1]
-var array : [Int] = []
-var result = [Int]()
+let N = Int(readLine()!)!
 
+var arr : [Int] = []
 
-var count = 0
+var result : [Int] = []
 
-array = Array((1...queue[0]))
-
-// 끝을 내야하니까 .
-
-while(!array.isEmpty){
-    //삭제로직.\
-    //일단 맨처음에는 3번째를 지우고,,
-    count += array[1]
-    count = count % array.count
-    result.append(array.remove(at:count ))
+for i in 1...4 {
+    
+    arr.append(i)
+    
 }
 
-print("<" + result.map{String($0)}.joined(separator: ", ") + ">")
 
+while(result.count == 1){
+    
+    arr.remove(at: arr.first!)
+    result = arr
+}
 
-
-
+print(result)
