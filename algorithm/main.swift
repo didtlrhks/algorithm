f69aed05d77db9835876cdc0d0a6d6dfd9cca0c5
sikwan <yysskk99999@naver.com>
2024-10-24 16:55:57 +0900

    1158

--- algorithm/main.swift
+++ algorithm/main.swift
@@ -7,9 +7,31 @@
 
 import Foundation
 
+let queue = readLine()!.split(separator: " ").map{Int($0)!}
 
-var N = readLine()!.split(separator: " ")
-//print(N)
-var M = Int(readLine()!)!
 
-var graph = [[Int]](repeating:[], count: <#T##Int#>)
+var N = queue[0]
+var K = queue[1]
+var array : [Int] = []
+var result = [Int]()
+
+
+var count = 0
+
+array = Array((1...queue[0]))
+
+// 끝을 내야하니까 .
+
+while(!array.isEmpty){
+    //삭제로직.\
+    //일단 맨처음에는 3번째를 지우고,,
+    count += array[1]
+    count = count % array.count
+    result.append(array.remove(at:count ))
+}
+
+print("<" + result.map{String($0)}.joined(separator: ", ") + ">")
+
+
+
+
