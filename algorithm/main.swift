21344082d4e93541bbd835869d5dea91f4a40cd5
sikwan <yysskk99999@naver.com>
2024-10-26 14:25:40 +0900

    9934

--- algorithm/main.swift
+++ algorithm/main.swift
@@ -6,36 +6,24 @@
 //
 
 import Foundation
-//입출력 예시
-//4 2
-//1 2 3 4
 
 
-for _ in 0 ..< Int(readLine()!)! {
-    let nm = readLine()!.split(separator: " ").map { Int(String($0))! } // 왜 String 을 받아서 Int 로 변환하는거지?
-    var importancies = readLine()!.split(separator: " ").map { Int(String($0))! }
-    if importancies.count == 1 {
-        print(1)
-        continue
-    }
-    var currentPoint = nm[1]
-    var count = 0
-    
-    while true {//while 문이 이해가 안됨
-        
-        if importancies.first! == importancies.max()! {// 여기서 느낌표가 언래핑이구나
-            count += 1
-            importancies.removeFirst()
-            if currentPoint == 0 {
-                break
-            } else {
-                currentPoint -= 1
-            }
-        } else {
-            importancies.append(importancies.first!)
-            importancies.removeFirst()
-            currentPoint = currentPoint == 0 ? importancies.count - 1 : currentPoint - 1
-        }
-    }
-    print(count)
+
+let K = Int(readLine()!)!
+var list =  readLine()!.split(separator:" " ).map{Int(String($0))!}
+
+var answer = Array(repeating: [Int](), count: K)
+
+func find(_ left: Int, _ right: Int, level: Int ) {
+    if left>right{ return }
+    let mid = (left+right)/2
+    answer[level].append(list[mid])
+    find(left, mid-1, level: level+1)
+    find(mid+1, right, level: level+1)
 }
+find(0, list.count-1, level: 0)
+answer.forEach{
+    $0.forEach{print($0, terminator: " ")}
+    print()
+}
+
